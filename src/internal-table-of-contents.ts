762ffd83dd97f359472181c1554e2a18e1594dc6
Philip Smolen <philip@trade-ideas.com>
2023-04-08 09:35:28 -0700

    The table of contents works now.

--- src/internal-table-of-contents.ts
+++ src/internal-table-of-contents.ts
@@ -1,5 +1,27 @@
 export {}
 
+import { getById } from "phil-lib/client-misc";
 import "./internal-table-of-contents.css";
 
-console.log("hi")
+const tocDiv = getById("toc", HTMLDivElement);
+const mainDiv = getById("main", HTMLDivElement);
+
+Array.from(mainDiv.querySelectorAll(".section, .article, .heading")).forEach(element => {
+  if (!(element instanceof HTMLElement)) {
+    console.warn("unexpected", element)
+  } else {
+    const tocName = element.innerText.replaceAll(/\./g, "");
+    console.log(tocName)
+    const tocClass = element.classList.contains("section")?"toc-2":"toc-1";
+    const tocA = document.createElement("a");
+    tocA.innerText = tocName;
+    tocA.classList.add(tocClass);
+    const name = element.dataset.name ?? tocName.replaceAll(/ /g, "_");
+    tocA.href = "#" + name;
+    tocDiv.appendChild(tocA);
+    const destinationA = document.createElement("a");
+    destinationA.id = name;
+    element.parentElement!.insertBefore(destinationA, element);
+    destinationA.appendChild(element);
+  }
+})
