6087b4133e98548847548fb92102e1c2de0843ff
Philip Smolen <philip@trade-ideas.com>
2023-04-08 10:50:58 -0700

    Improve the hyperlinks.
    Each section header now includes a link to itself.
    If you click on the section header, the # in your browser will change.
    You can copy that URL with a pointer directly to that section.

--- src/internal-table-of-contents.ts
+++ src/internal-table-of-contents.ts
@@ -1,4 +1,4 @@
-export {}
+export {};
 
 import { getById } from "phil-lib/client-misc";
 import "./internal-table-of-contents.css";
@@ -6,22 +6,28 @@ import "./internal-table-of-contents.css";
 const tocDiv = getById("toc", HTMLDivElement);
 const mainDiv = getById("main", HTMLDivElement);
 
-Array.from(mainDiv.querySelectorAll(".section, .article, .heading")).forEach(element => {
-  if (!(element instanceof HTMLElement)) {
-    console.warn("unexpected", element)
-  } else {
-    const tocName = element.innerText.replaceAll(/\./g, "");
-    console.log(tocName)
-    const tocClass = element.classList.contains("section")?"toc-2":"toc-1";
-    const tocA = document.createElement("a");
-    tocA.innerText = tocName;
-    tocA.classList.add(tocClass);
-    const name = element.dataset.name ?? tocName.replaceAll(/ /g, "_");
-    tocA.href = "#" + name;
-    tocDiv.appendChild(tocA);
-    const destinationA = document.createElement("a");
-    destinationA.id = name;
-    element.parentElement!.insertBefore(destinationA, element);
-    destinationA.appendChild(element);
+Array.from(mainDiv.querySelectorAll(".section, .article, .heading")).forEach(
+  (element) => {
+    if (!(element instanceof HTMLElement)) {
+      console.warn("unexpected", element);
+    } else {
+      const tocName = element.innerText.replaceAll(/\./g, "");
+      const tocClass = element.classList.contains("section")
+        ? "toc-2"
+        : "toc-1";
+      const tocA = document.createElement("a");
+      tocA.innerText = tocName;
+      tocA.classList.add(tocClass);
+      const name = element.dataset.name ?? tocName.replaceAll(/ /g, "_");
+      const href = "#" + name;
+      tocA.href = href;
+      tocDiv.appendChild(tocA);
+      const destinationA = document.createElement("a");
+      destinationA.id = name;
+      destinationA.href = href;
+      destinationA.className = "self-pointer";
+      element.parentElement!.insertBefore(destinationA, element);
+      destinationA.appendChild(element);
+    }
   }
-})
+);
