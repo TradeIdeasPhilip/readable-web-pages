2886996b6a53332abb1fb2b2903d73d5895e6d08
Philip Smolen <philip@trade-ideas.com>
2023-04-19 15:03:04 -0500

    Reorganize the GUI.
    Add some help and pointers for the end user.
    Add lots of comments and reorganized the code.

--- src/continuous-font-weight.ts
+++ src/continuous-font-weight.ts
@@ -1,10 +1,12 @@
+import { getById } from "phil-lib/client-misc";
 import "./continuous-font-weight.css";
+
+const insertSamplesHere = getById("insertSamplesHere", HTMLDivElement);
 
 for (let fontWeight = 200; fontWeight <= 900; fontWeight += 50) {
   const div = document.createElement("div");
   const realWeight = (fontWeight || 1).toString();
   div.style.fontWeight = realWeight;
-  div.style.fontSize = "33.333%";
-  div.innerText = `* \\-/| div.style.fontWeight = ${realWeight}`;
-  document.body.appendChild(div);
+  div.innerText = `font-weight: ${realWeight}; * \\-/| The quick brown fox jumps over the lazy dog!`;
+  insertSamplesHere.appendChild(div);
 }
