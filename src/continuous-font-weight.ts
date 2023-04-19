b6c1dbfe388fbad1a222a759c7895aea8377232a
Philip Smolen <philip@trade-ideas.com>
2023-04-18 19:15:22 -0500

    Change the font size again.

--- src/continuous-font-weight.ts
+++ src/continuous-font-weight.ts
@@ -4,7 +4,7 @@ for (let fontWeight = 200; fontWeight <= 900; fontWeight += 50) {
   const div = document.createElement("div");
   const realWeight = (fontWeight || 1).toString();
   div.style.fontWeight = realWeight;
-  div.style.fontSize = "50%";
+  div.style.fontSize = "33.333%";
   div.innerText = `* \\-/| div.style.fontWeight = ${realWeight}`;
   document.body.appendChild(div);
 }
