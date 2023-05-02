968e892e75a964d913452c8af5adce9616056eb6
Philip Smolen <philip@trade-ideas.com>
2023-05-01 19:40:45 -0500

    Add both copies of the table of contents.
    Internal release.
    TODO When I select vertical the table of contents gets tiny.
    TODO fix that.
    
    Improve some formatting.

--- src/unlimited-scroll.ts
+++ src/unlimited-scroll.ts
@@ -5,6 +5,7 @@ import "./unlimited-scroll.css";
 
 const tocDiv = getById("toc", HTMLDivElement);
 const mainDiv = getById("main", HTMLDivElement);
+const tocIndependentBody = getById("toc-independent-body", HTMLDivElement);
 
 /*
   I never quite got this to work.  I've done similar things in the past,
@@ -66,65 +67,65 @@ Array.from(mainDiv.querySelectorAll(".section, .article, .heading")).forEach(
       const isSection = element.classList.contains("section");
       const tocClass = isSection ? "toc-2" : "toc-1";
       /**
-       * This is the anchor element we are creating and adding to the table of contents.
-       */
-      const tocA = document.createElement("a");
-      tocA.innerText = tocName;
-      tocA.classList.add(tocClass);
-      /**
        * I think "name" was the name of an attribute of an anchor in html 4, but now you use the id attribute instead.
        *
        * This will be the value of the fragment, the part of the url found after the #.
        */
-      let name = element.dataset.name;
-      if (!name) {
-        name = tocName.replaceAll(/ /g, "_");
+      let anchorId = element.dataset.name;
+      if (!anchorId) {
+        anchorId = tocName.replaceAll(/ /g, "_");
         if (isSection) {
           // Section names repeat.  So prefix the name with the name of the most recent article.
           // This will produce a name like "Article_II_Section_1"
-          name = hashPrefix + "_" + name;
+          anchorId = hashPrefix + "_" + anchorId;
         } else {
           // Save this name in case.  hasPrefix will have a value like "Article_II".
-          hashPrefix = name;
+          hashPrefix = anchorId;
         }
       }
       /**
        * Something like "#Article_II_Section_1".
        */
-      const href = "#" + name;
-      tocA.href = href;
-      tocDiv.appendChild(tocA);
-      const destinationA = document.createElement("a");
-      destinationA.id = name;
-      destinationA.href = href;
-      destinationA.className = "self-pointer";
-      element.parentElement!.insertBefore(destinationA, element);
-      destinationA.appendChild(element);
-      if (href == initiallyRequestedHash) {
-        // This is not automatic because the browser tries to scroll into view before we've created the <a>.
-        // We simulate it and the user won't know the difference.
-        destinationA.scrollIntoView();
+      const href = "#" + anchorId;
+      for (const container of [tocDiv, tocIndependentBody]) {
+        /**
+         * This is the anchor element we are creating and adding to the table of contents.
+         */
+        const tocA = document.createElement("a");
+        tocA.innerText = tocName;
+        tocA.classList.add(tocClass);
+        tocA.href = href;
+        container.appendChild(tocA);
+        const destinationA = document.createElement("a");
+        destinationA.id = anchorId;
+        destinationA.href = href;
+        destinationA.className = "self-pointer";
+        element.parentElement!.insertBefore(destinationA, element);
+        destinationA.appendChild(element);
+        if (href == initiallyRequestedHash) {
+          // This is not automatic because the browser tries to scroll into view before we've created the <a>.
+          // We simulate it and the user won't know the difference.
+          destinationA.scrollIntoView();
+        }
       }
     }
   }
 );
 
-/**
- * Eventually this should be dynamic.  In case the
- */
-const shouldScrollToc = false;
-
 function updateTocSelection() {
   const hash = location.hash;
-  Array.from(tocDiv.querySelectorAll("a")).forEach((anchor) => {
-    // Note:  anchor.href returns a string with an absolute url.
-    // getAttribute() returns the original value we stored into that field, which will start with a #.
-    const isSelected = anchor.getAttribute("href") == hash;
-    const action = isSelected ? "add" : "remove";
-    anchor.classList[action]("toc-selected");
-    if (isSelected && shouldScrollToc) {
-      anchor.scrollIntoView();
-    }
+  [tocDiv, tocIndependentBody].forEach((container) => {
+    const shouldScrollToc = container == tocIndependentBody;
+    Array.from(container.querySelectorAll("a")).forEach((anchor) => {
+      // Note:  anchor.href returns a string with an absolute url.
+      // getAttribute() returns the original value we stored into that field, which will start with a #.
+      const isSelected = anchor.getAttribute("href") == hash;
+      const action = isSelected ? "add" : "remove";
+      anchor.classList[action]("toc-selected");
+      if (isSelected && shouldScrollToc) {
+        anchor.scrollIntoView();
+      }
+    });
   });
 }
 
@@ -171,8 +172,10 @@ const horizontalScrollButton = getById("horizontalScroll", HTMLButtonElement);
  */
 const scrollerTop = getById("main", HTMLDivElement);
 
-function isValidScrollType(input : string | undefined) : input is ("vertical" | "horizontal") {
-  return (input == "vertical") || (input == "horizontal");
+function isValidScrollType(
+  input: string | undefined
+): input is "vertical" | "horizontal" {
+  return input == "vertical" || input == "horizontal";
 }
 
 /**
